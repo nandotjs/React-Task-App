60fa23921aa60237f8aac73d4ee71b239c3ca653
HennrikeSocher <socherhennrike@gmail.com>
2024-04-24 01:30:00 -0300

    fix hovers

--- src/app/components/task-modal.tsx
+++ src/app/components/task-modal.tsx
@@ -62,8 +62,8 @@ const TaskList: React.FC = () => {
           <div key={index} className={`flex justify-between items-center bg-gray-100 p-4 mb-2 rounded border ${task.includes('(Feito)') ? 'border-green-500' : 'border-gray-600'}`}>
             <span className={`flex-grow ${task.includes('(Feito)') && 'line-through'}`}>{task}</span>
             <div className="flex">
-              <button className="text-white bg-red-500 hover:bg-gray-800 hover:text-white font-medium py-1 px-2 rounded-md mr-2 transition duration-300 ease-in-out" onClick={() => handleDeleteTask(index)}>Excluir</button>
-              <button className={`text-white font-medium py-1 px-2 rounded-md transition duration-300 ease-in-out ${task.includes('(Feito)') ? 'bg-red-500 hover:bg-red-700' : 'bg-green-500 hover:bg-gray-800'}`} onClick={() => handleToggleTask(index)}>
+              <button className="text-white bg-red-500 hover:bg-red-700 hover:text-white font-medium py-1 px-2 rounded-md mr-2 transition duration-300 ease-in-out" onClick={() => handleDeleteTask(index)}>Excluir</button>
+              <button className={`text-white font-medium py-1 px-2 rounded-md transition duration-300 ease-in-out ${task.includes('(Feito)') ? 'bg-red-500 hover:bg-red-700' : 'bg-green-500 hover:bg-green-800'}`} onClick={() => handleToggleTask(index)}>
                 {task.includes('(Feito)') ? 'Desfazer' : 'Feito'}
               </button>
             </div>
