7fe504ca2f277262b19ca884bf9ad521c793c31a
HennrikeSocher <socherhennrike@gmail.com>
2024-04-24 01:00:47 -0300

    feat: botao de logout e marcar task feita

--- src/app/components/task-modal.tsx
+++ src/app/components/task-modal.tsx
@@ -2,30 +2,6 @@
 
 import React, { useState } from 'react';
 
-// Definição de tipo para o parâmetro text
-interface TaskProps {
-  text: string;
-  onDelete: () => void;
-  onToggle: () => void;
-}
-
-
-// Componente de tarefa individual
-const Task: React.FC<TaskProps> = ({ text, onDelete, onToggle }) => {
-  return (
-    <div className="flex justify-between items-center bg-gray-100 p-4 mb-2 rounded border border-gray-600">
-      <span className={`flex-grow ${text.includes('(Feito)') && 'line-through'}`}>{text}</span>
-      <div className="flex">
-        <button className="text-white bg-red-500 hover:bg-gray-800 hover:text-white font-medium py-1 px-2 rounded-md mr-2 transition duration-300 ease-in-out" onClick={onDelete}>Excluir</button>
-        <button className="text-white bg-green-500 hover:bg-gray-800 hover:text-white font-medium py-1 px-2 rounded-md transition duration-300 ease-in-out" onClick={onToggle}>Feito</button>
-      </div>
-    </div>
-  );
-};
-
-
-
-// Componente de lista de tarefas
 const TaskList: React.FC = () => {
   const [tasks, setTasks] = useState<string[]>([]);
   const [newTask, setNewTask] = useState<string>('');
@@ -52,33 +28,45 @@ const TaskList: React.FC = () => {
     setTasks(updatedTasks);
   };
 
+  // Função para fazer logout
+  const handleLogout = () => {
+    // Adicione a lógica de logout aqui, por exemplo, limpar o token de autenticação
+
+    // Redirecionamento para a tela de login
+    window.location.href = '/';
+  };
+
   return (
     <div className="max-w-md mx-auto mt-8">
-      {/* Componente para adicionar nova tarefa */}
-      <div className="mb-4 flex justify-center">
-        <input
-          className="border border-gray-400 p-2 mr-2 w-64"
-          type="text"
-          placeholder="Adicionar nova tarefa"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleAddTask}
-        >
-          Adicionar
+      <div className="flex justify-between mb-4">
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleLogout}>
+          Logout
         </button>
+        <div className="flex justify-center items-center">
+          <input
+            className="border border-gray-400 p-2 mr-2 w-64 ml-2"
+            type="text"
+            placeholder="Adicionar nova tarefa"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleAddTask}
+          >
+            Adicionar
+          </button>
+        </div>
       </div>
-      {/* Lista de tarefas */}
       <div>
         {tasks.map((task, index) => (
-          <Task
-            key={index}
-            text={task}
-            onDelete={() => handleDeleteTask(index)}
-            onToggle={() => handleToggleTask(index)}
-          />
+          <div key={index} className={`flex justify-between items-center bg-gray-100 p-4 mb-2 rounded border ${task.includes('(Feito)') ? 'border-green-500' : 'border-gray-600'}`}>
+            <span className={`flex-grow ${task.includes('(Feito)') && 'line-through'}`}>{task}</span>
+            <div className="flex">
+              <button className="text-white bg-red-500 hover:bg-gray-800 hover:text-white font-medium py-1 px-2 rounded-md mr-2 transition duration-300 ease-in-out" onClick={() => handleDeleteTask(index)}>Excluir</button>
+              <button className="text-white bg-green-500 hover:bg-gray-800 hover:text-white font-medium py-1 px-2 rounded-md transition duration-300 ease-in-out" onClick={() => handleToggleTask(index)}>Feito</button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
@@ -86,3 +74,4 @@ const TaskList: React.FC = () => {
 };
 
 export default TaskList;
+
